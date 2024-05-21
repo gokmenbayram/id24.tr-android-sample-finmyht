16c62cbd02d2b7985fecb37f0988ac314f6699bc
filipraj <filip.raj@credeo.rs>
2024-05-21 11:23:32 +0200

    clone from old repository

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# id24.tr-android-sample
+# IdentifySampleApp
+
+In the MainActivity, you need to fill in Api Url, socket url/port, stun url/port, turn url/port/username/password and ident id information.
+<pre>
+          val identifyObject = IdentifySdk.Builder()
+            .api("api url")
+            .socket("socket url","socket port")
+            .stun("stun url","stun port")
+            .turn("turn url","turn port","turn username","turn password")
+            .lifeCycle(this.lifecycle)
+            .options(identityOptions)
+            .build()
+
+        identifyObject.startIdentification(this,"xxxx-xxxx-xxxx-xxxx-xxxxxxx","tr")
+</pre>
+
+
+In settings.gradle, the value value in the credentials section must be filled for your access to the SDK. If you do not have this data, please contact us.
+
+
+ <pre>allprojects {
+repositories {
+...
+  maven {
+            url = 'https://maven.pkg.github.com/business-service-solution-gmbh/id24.tr-android-sdk'
+            name = "GitHubPackages"
+            credentials {
+                username = "bssserviceacc"
+                password = "xxxxxx"
+            }
+        }
+}
+}</pre>
---
+++ app/src/main/java/com/identify/identifysampleapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.identify.identifysampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.identify.sdk.IdentifySdk
+import com.identify.sdk.IdentityOptions
+import com.identify.sdk.repository.model.enums.IdentifyModuleTypes
+import com.identify.sdk.repository.model.mrz.DocType
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val identityOptions = IdentityOptions.Builder()
+            .setIdentityType(
+                listOf(
+                    IdentifyModuleTypes.PREPARE,
+                    IdentifyModuleTypes.SPEECH_TEST,
+                    IdentifyModuleTypes.IDENTIFICATION_INFORMATION_WITH_CARD_PHOTO,
+                    IdentifyModuleTypes.IDENTIFICATION_INFORMATION_WITH_NFC,
+                    IdentifyModuleTypes.TAKE_SELFIE,
+                    IdentifyModuleTypes.VALIDATE_ADDRESS,
+                    IdentifyModuleTypes.LIVENESS_TEST,
+                    IdentifyModuleTypes.VIDEO_RECORD,
+                    IdentifyModuleTypes.SIGNATURE,
+                    IdentifyModuleTypes.AGENT_CALL
+                )
+            )
+            .setNfcExceptionCount(3)
+            .setCallConnectionTimeOut(20000)
+            .setOpenIntroPage(false)
+            .setDocumentType(DocType.ID_CARD)
+            .setOpenThankYouPage(false)
+            .setVideoRecordTime(5000)
+            .setCallConnectionTimeOut(5000)
+            .setAutoSelfieWithLivenessDetection(false)
+            .setEnableFaceControlInSelfie(true)
+            .setEnableLightInformationInCall(true)
+            .build()
+
+
+        val identifyObject = IdentifySdk.Builder()
+            .api("api url")
+            .lifeCycle(this.lifecycle)
+            .options(identityOptions)
+            .build()
+
+        identifyObject.startIdentification(this,"xxxx-xxxx-xxxx-xxxx-xxxxxxx","tr")
+    }
+}
